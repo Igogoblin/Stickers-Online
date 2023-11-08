280e8ba4460bad5a3515bacd1e52cb90d1117f8c
Ihar Skavysh <igogoblin@gmail.com>
2023-11-08 14:19:19 +0300

    feat: add base structure

---
+++ stickers/src/components/Sticker.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Stiecker() {
+  return <div>stiecker</div>;
+}
+
+export default Stiecker;
---
+++ stickers/src/store/stickerSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stickerSlice = createSlice({
+  name: "stieck",
+  initialState: {
+    stieckers: [],
+  },
+  reducers: {},
+});
